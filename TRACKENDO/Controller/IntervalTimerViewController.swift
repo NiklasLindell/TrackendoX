f4fd2bc50c42b29c9f7f86c3c8be8cc4d5d614ba
matildadahlberg <info.matildadahlberg@gmail.com>
2018-04-17 14:13:47 +0200

    Klockan är typ klar, förutom ljudet. :P

--- TRACKENDO/Controller/IntervalTimerViewController.swift
+++ TRACKENDO/Controller/IntervalTimerViewController.swift
@@ -9,13 +9,10 @@ class IntervalTimerViewController: UIViewController {
     @IBOutlet weak var runVSrestLbl: UILabel!
     
     @IBOutlet weak var runTextField: UILabel!
-    
     @IBOutlet weak var restTextField: UILabel!
-    
     @IBOutlet weak var roundsTextField: UILabel!
     
     @IBOutlet weak var runSliderOutlet: UISlider!
-    
     @IBOutlet weak var roundSliderOutlet: UISlider!
     @IBOutlet weak var restSliderOutlet: UISlider!
     
@@ -25,11 +22,12 @@ class IntervalTimerViewController: UIViewController {
     @IBOutlet weak var stopOutlet: UIButton!
     @IBOutlet weak var pauseOutlet: UIButton!
     
-    var audioPlayer = AVAudioPlayer()
+    var audioPlayer : AVAudioPlayer?
     var timer = Timer()
     var runTime  = 0
     var restTime = 0
     var rounds = 0
+    var timePassed = 0
     var pause = false
     
     override func viewDidLoad() {
@@ -45,19 +43,17 @@ class IntervalTimerViewController: UIViewController {
         stopOutlet.layer.borderWidth = 1
         pauseOutlet.layer.borderWidth = 1
         
-        
         runTime = Int((runSliderOutlet.maximumValue - runSliderOutlet.minimumValue) * 0.5)
         restTime = Int((restSliderOutlet.maximumValue - restSliderOutlet.minimumValue) * 0.5)
         rounds = Int((roundSliderOutlet.maximumValue - roundSliderOutlet.minimumValue) * 0.5)
         
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "1beepAlarm", ofType: "mp3")!))
-            audioPlayer.prepareToPlay()
-        } catch{
-            print(error)
+        if let url = Bundle.main.url(forResource: "1beepAlarm", withExtension: "wav") {
+            audioPlayer = try? AVAudioPlayer(contentsOf: url)
+        } else {
+            print("file not found ERROR")
         }
     }
-
+    
     //slidern som ändrar "run" sekundrarna
     @IBAction func runSlider(_ sender: UISlider) {
         runTime = Int(sender.value)
@@ -101,7 +97,11 @@ class IntervalTimerViewController: UIViewController {
     @objc func counter() {
         if rounds > 0 {
             
-            totalTime.text = String((runTime + restTime) * rounds) + " sec"
+            let tot = (Int(runSliderOutlet.value) + Int(restSliderOutlet.value)) * Int(roundSliderOutlet.value)
+            
+            print("Run: \(runTime) Rest: \(restTime) Rounds: \(rounds)  Total: \(tot - timePassed)")
+            
+            totalTime.text = String(tot - timePassed )  + " sec"
             if (runTime + restTime) * rounds >= 60 {
                 totalTime.text = String(((runTime + restTime) * rounds)/60) + " min"
             }
@@ -111,39 +111,38 @@ class IntervalTimerViewController: UIViewController {
                 runVSrestLbl.textColor = UIColor.green
                 timeLbl.text = String(runTime)
                 runTime -= 1
-                
+                timePassed += 1
             }
+                
             else if (restTime > 0 && runTime == 0){
                 runVSrestLbl.text = "REST"
                 runVSrestLbl.textColor = UIColor.red
                 timeLbl.text = String(restTime)
                 restTime -= 1
-                
+                timePassed += 1
             }
-            else if (runTime <= 0 && restTime <= 0) {
+            
+            if (runTime <= 0 && restTime <= 0) {
                 rounds -= 1
                 runTime = Int(runSliderOutlet.value)
                 restTime = Int(restSliderOutlet.value)
-                
-            }
-//            else if (restTime <= 0 && rounds == 1){
-//                audioPlayer.play()
-//                timeLbl.text = "0"
-//                runVSrestLbl.text = "Activity"
-//                startOutlet.isEnabled = true
-//                timer.invalidate()
-//                audioPlayer.stop()
-//            }
-            else {
-                print("hej")
-                timer.invalidate()
-                startOutlet.isEnabled = true
-                startOutlet.layer.backgroundColor = UIColor.clear.cgColor
-                stopOutlet.layer.backgroundColor = UIColor.clear.cgColor
-                pauseOutlet.layer.backgroundColor = UIColor.clear.cgColor
+                audioPlayer?.play()
             }
         }
+            
+        else {
+            timer.invalidate()
+            timeLbl.text = "0"
+            totalTime.text = "0"
+            timePassed = 0
+            runVSrestLbl.text = "Activity"
+            startOutlet.isEnabled = true
+            startOutlet.layer.backgroundColor = UIColor.clear.cgColor
+            stopOutlet.layer.backgroundColor = UIColor.clear.cgColor
+            pauseOutlet.layer.backgroundColor = UIColor.clear.cgColor
+        }
     }
+    
     //stop-knappen som stoppar klockan
     @IBAction func StopBtn(_ sender: Any) {
         timer.invalidate()
@@ -174,7 +173,6 @@ class IntervalTimerViewController: UIViewController {
         stopOutlet.layer.backgroundColor = UIColor.clear.cgColor
         pauseOutlet.layer.backgroundColor = UIColor.darkGray.cgColor
     }
- 
 }
 
 
